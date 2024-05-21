cfd9ec85874b93162ccc3b9ae7046d90b2ad2769
Zayleth <zaylethvegas.com>
2024-05-20 22:28:25 -0400

    3 variables exercises in JS

--- Java_Script/variables.js
+++ Java_Script/variables.js
@@ -15,21 +15,38 @@
 
 // NOTE - Variables should be named in a way that allows us to easily understand what’s inside them.
 
-// TASKS - Working with variables
-// Declare two variables: admin and name.
-// Assign the value "John" to name.
-// Copy the value from name to admin.
-// Show the value of admin using alert (must output “John”).
+// TASKS
 
-
+// Working with variables
+// 1. Declare two variables: admin and name.
+// 2. Assign the value "John" to name.
+// 3. Copy the value from name to admin.
+// 4. Show the value of admin using alert (must output “John”).
 
 let name = "John";
-
 let admin = 'name';
 
 alert( admin ); // "John"
 
-console.log (admin);
+// Giving the right name
+// 1. Create a variable with the name of our planet. How would you name such a variable?
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
 
+let eart_planet = '';
+let current_visitor_name = '';
 
+// Uppercase const?
+// 1. Examine the following code:
 
+// const birthday = '18.04.1982';
+// const age = someCode(birthday);
+
+/*
+Here we have a constant birthday for the date, and also the age constant.
+Would it be right to use upper case for birthday? For age? Or even for both?
+
+const BIRTHDAY = '18.04.1982'; // make birthday uppercase?
+const AGE = someCode(BIRTHDAY); // make age uppercase?
+*/
+
+const BIRTHDAY = '18.04.1982'; // constant value that does not change over time.
