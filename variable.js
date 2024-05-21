4eafa085df53b289fbf6250d686859e0e3bbb0f3
Zayleth <zaylethvegas.com>
2024-05-20 20:43:21 -0400

    JS exercises - variables

---
+++ variable.js
@@ -0,0 +1,35 @@
+// A variable is a "named storage" for data. We can use variables to store data from our code.
+// Each variable has a label with a unique name
+// To create a variable in JavaScript; 
+// let = 'keyword'; is a modern variable declaration.
+// var = 'variable_declaration'; is an old-school variable declaration. Normally we don’t use it at all. it has its differences with let
+// const = 'value that does not change or constant value'
+
+// Variables can have any name assigned to them, except reserved names specified by Java Script. These are
+
+    //let
+    //class
+    //return
+    //functions
+
+
+// NOTE - Variables should be named in a way that allows us to easily understand what’s inside them.
+
+// TASKS - Working with variables
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+
+
+
+let name = "John";
+
+let admin = 'name';
+
+alert( admin ); // "John"
+
+console.log (admin);
+
+
+
