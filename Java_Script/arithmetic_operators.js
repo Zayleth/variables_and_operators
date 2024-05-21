4a7d4aeea31110c82b0bbfdb20a41de59f0da5fd
Zayleth <zaylethvegas.com>
2024-05-20 21:06:32 -0400

    JS Operator Exercises

---
+++ Java_Script/arithmetic_operators.js
@@ -0,0 +1,26 @@
+// Arithmetic Operations
+// A typical arithmetic operation operates on two numbers.
+
+/*  Operator	Description
+
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus
+++	Increment
+--	Decrement 
+
+*/
+
+// Exercise:
+// Multiply 10 with 5, and alert the result:
+
+alert(10 * 5);
+
+//Divide 10 by 2, and alert the result.
+
+//alert(10 ___ 2);
+
+alert(10/2);
