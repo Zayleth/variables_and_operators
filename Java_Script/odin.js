225233ecbd961a5c93bb8c75993eb3080e045c47
Zayleth <zaylethvegas.com>
2024-05-23 16:26:09 -0400

    Variable activities from Odin (5)

---
+++ Java_Script/odin.js
@@ -0,0 +1,44 @@
+// ASSIGNMENT
+
+// 1. Add 2 numbers together! (just type console.log(23 + 97) into your HTML file)
+console.log (32 + 45);
+
+
+
+// 2. Add a sequence of 6 different numbers together.
+console.log (45, 85, 20, 36, 10, 74);
+
+
+
+// 3. Print the value of the following expression: (4 + 6 + 9) / 77
+console.log ((4 + 6 + 9) / 77); // 0.24675324675324675
+
+
+
+// 4. Let’s use variables! Type this statement at the top of the script tag: 
+let a = 10;
+console.log(a); // In the console console.log(a) should print 10
+
+// Try the following in the console: 
+console.log(9 * a); // 90
+
+//and this: 
+let b = 7 * a // (returns undefined *) // 70
+
+// and then console.log(b)
+console.log(b); // 70
+
+
+
+// 5. You should be getting the hang of this by now… try this sequence:
+// Declare a constant variable MAX with the value 57
+const MAX = 57;
+
+// Set another variable actual to MAX - 13
+let actual = MAX - 13;
+
+// Set another variable percentage to actual / MAX
+let persentage = actual / MAX;
+
+// If you type percentage in the console and press Enter you should see a value like 0.7719
+// percentage = 0.7719298245614035
